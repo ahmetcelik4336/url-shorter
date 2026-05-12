614ad6116eb79f90378575e9eb781e957f2f0aea
ahmetcelik4336 <ahmet.celik5443@gmail.com>
2026-05-12 22:18:03 +0300

    render.com

--- api/internal/database/ent.go
+++ api/internal/database/ent.go
@@ -15,7 +15,7 @@ var Client *ent.Client
 func Init() (*ent.Client, string) {
 
 	driver := os.Getenv("DATABASE_DRIVER")
-	dsn := os.Getenv("DATABASE_URL_MYSQL")
+	dsn := os.Getenv("DATABASE_URL")
 
 	client, err := ent.Open(driver, dsn)
 	if err != nil {
